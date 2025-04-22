d26ad48791b6978daabe44ab6ece6c07fb7569e1
ReirLair <indiaserverff32@gmail.com>
2025-04-22 18:57:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ module.exports = {
   ownerName: process.env.OWNER_NAME || 'Reiker',
   contact: process.env.CONTACT || '3584573986503@s.whatsapp.net',
   user: process.env.BOT_USER || 'Reiker',
-  autoStatus: true, // Set to false to stop auto view status
+  autoStatus: true, // Set to false to stop auto view status 
   OWNER: ownerWithDomain,
   prefix: process.env.PREFIX || '!',
   thumbnail: process.env.THUMBNAIL || '/home/container/user_data/7365381557/Test/thumbnail.jpg',
-  SESSION_ID: process.env.SESSION_ID || '', // Put session id in the '' before the , paste session id it it
+  SESSION_ID: process.env.SESSION_ID || '', // Put session id in the '' before the , paste session id it Or Put your number in contact.json for terminal pairing
   port: process.env.PORT || 3000 // Added port configuration with default 3000
 };
