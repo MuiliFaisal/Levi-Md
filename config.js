1019f6dfcfa32fbe5c3bd1f229e600a9833055e7
ReirLair <indiaserverff32@gmail.com>
2025-04-23 06:45:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,12 @@ module.exports = {
   ownerName: process.env.OWNER_NAME || 'Reiker',
   contact: process.env.CONTACT || '3584573986503@s.whatsapp.net',
   user: process.env.BOT_USER || 'Reiker',
-  autoStatus: true, // Set to false to stop auto view status 
+  autoStatus: true, // Set to false to stop auto view status
   OWNER: ownerWithDomain,
   prefix: process.env.PREFIX || '!',
   thumbnail: process.env.THUMBNAIL || '/home/container/user_data/7365381557/Test/thumbnail.jpg',
-  SESSION_ID: process.env.SESSION_ID || '', // Put session id in the '' before the , paste session id it Or Put your number in contact.json for terminal pairing
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '3584573986503',
+  SESSION_ID: process.env.SESSION_ID || '', // Put session id in the '' before the , paste session id it it
+  presence: process.env.PRESENCE || '', // can be 'available' 'recording' 'composing' composing for typing 'unavailable' for offline
   port: process.env.PORT || 3000 // Added port configuration with default 3000
 };
