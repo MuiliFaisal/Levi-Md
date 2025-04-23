ba01048911deeecbd50db74789f43bbe63b8021b
ReirLair <indiaserverff32@gmail.com>
2025-04-23 06:46:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const ownerRaw = process.env.CONTACT || '2340000000000';
+const ownerRaw = process.env.CONTACT || '2348012345678';
 const ownerWithDomain = ownerRaw.includes('@s.whatsapp.net') 
   ? ownerRaw 
   : `${ownerRaw}@s.whatsapp.net`;
@@ -8,13 +8,13 @@ const ownerWithDomain = ownerRaw.includes('@s.whatsapp.net')
 module.exports = {
   botName: process.env.BOT_NAME || 'Kaizen',
   ownerName: process.env.OWNER_NAME || 'Reiker',
-  contact: process.env.CONTACT || '3584573986503@s.whatsapp.net',
+  contact: process.env.CONTACT || '2348012345678@s.whatsapp.net',
   user: process.env.BOT_USER || 'Reiker',
   autoStatus: true, // Set to false to stop auto view status
   OWNER: ownerWithDomain,
   prefix: process.env.PREFIX || '!',
   thumbnail: process.env.THUMBNAIL || '/home/container/user_data/7365381557/Test/thumbnail.jpg',
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '3584573986503',
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '2348012345678',
   SESSION_ID: process.env.SESSION_ID || '', // Put session id in the '' before the , paste session id it it
   presence: process.env.PRESENCE || '', // can be 'available' 'recording' 'composing' composing for typing 'unavailable' for offline
   port: process.env.PORT || 3000 // Added port configuration with default 3000
